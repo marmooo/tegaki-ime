f734d5f1f830648ae256aeb112ffb4dad4261e6d
marmooo <marmooo@users.noreply.github.com>
2023-09-17 09:13:19 +0900

    bump bootstrap from 5.3.1 to 5.3.2

--- src/sw.js
+++ src/sw.js
@@ -6,7 +6,7 @@ const urlsToCache = [
   "/tegaki-ime/model/model.json",
   "/tegaki-ime/model/group1-shard1of1.bin",
   "/tegaki-ime/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.6/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.11.0/dist/tf.min.js",
 ];
