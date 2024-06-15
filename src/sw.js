085d1cb864ab0016f4cf3f7c80ce540c1a5c13fe
marmooo <marmooo@users.noreply.github.com>
2024-06-15 10:57:03 +0900

    bump tfjs from 4.19.0 to 4.20.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-05-20 01:29";
+const CACHE_NAME = "2024-06-15 10:56";
 const urlsToCache = [
   "/tegaki-ime/",
   "/tegaki-ime/index.js",
@@ -6,7 +6,7 @@ const urlsToCache = [
   "/tegaki-ime/model/model.json",
   "/tegaki-ime/model/group1-shard1of1.bin",
   "/tegaki-ime/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.19.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.20.0/dist/tf.min.js",
 ];
 
 self.addEventListener("install", (event) => {
