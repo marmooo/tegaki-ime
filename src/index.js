ce65c2bd506eb784d1a9228db31ef1ed2c3857bf
marmooo <marmooo@users.noreply.github.com>
2026-09-20 16:57:53 +0900

    bump signature_pad from 5.1.3 to 5.1.4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.3/+esm";
+import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.4/+esm";
 
 const textarea = document.getElementById("textarea");
 const kanji4List = Array.from(
